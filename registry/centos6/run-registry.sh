6899e33a04d4638609971f416ac9dcf889cf7c48
smallfish <smallfish.xy@gmail.com>
2014-11-14 15:30:23 +0800

    Fix docker-registry run exception: No module named wsgi

--- registry/centos6/run-registry.sh
+++ registry/centos6/run-registry.sh
@@ -6,5 +6,5 @@ source /etc/sysconfig/docker-registry
 export SETTINGS_FLAVOR
 
 cd /usr/lib/python2.6/site-packages/docker-registry
-exec /usr/bin/gunicorn --access-logfile - --debug --max-requests 100 --graceful-timeout 3600 -t 3600 -k gevent -b ${REGISTRY_ADDRESS}:${REGISTRY_PORT} -w $GUNICORN_WORKERS wsgi:application
+exec /usr/bin/gunicorn --access-logfile - --debug --max-requests 100 --graceful-timeout 3600 -t 3600 -k gevent -b ${REGISTRY_ADDRESS}:${REGISTRY_PORT} -w $GUNICORN_WORKERS docker_registry.wsgi:application
 
