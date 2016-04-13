fd72b5d35544e7c00272f5edc63d75728b11152d
Mohammed Zeeshan Ahmed <moahmed@dhcp35-90.lab.eng.blr.redhat.com>
2016-04-13 21:34:32 +0530

    Added the CentOS dockerfile for cockpit-kubernetes
    This dockerfile creates a container that runs cockpit and can be plugged into
    kubernetes cluster. This is meant to go with the Atomic Registry dockerfile
    but can also be used with any kubernetes cluster.

---
+++ cockpit-kubernetes/centos7/install.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+set -ex
+
+# Install packages without dependencies
+if [ -z "$VERSION" ] && [ -z "$OFFLINE" ]; then
+    eval $(/container/scripts/get-version-env.sh)
+fi
+
+yum install cockpit-shell cockpit-ws cockpit-kubernetes -y;
+
+# Remove unwanted packages
+rm -rf /usr/share/cockpit/realmd/ /usr/share/cockpit/system/ /usr/share/cockpit/tuned/ /usr/share/cockpit/users/ /usr/share/cockpit/dashboard/
+
+rm -rf /container/scripts
+rm -rf /container/rpms
+
+# Openshift will change the user
+# but it will be part of gid 0
+# so make the files we need group writable
+rm -rf /etc/cockpit/
+mkdir -p /etc/cockpit/
+chmod 775 /etc
+chmod 775 /etc/cockpit
+chmod 775 /etc/os-release
+chmod 775 /usr/share/cockpit/shell
+chmod 775 /usr/share/cockpit/kubernetes
+
+# Move kubernetes index file away so we only link it when we want it
+mv /usr/share/cockpit/kubernetes/index.min.html.gz /usr/share/cockpit/kubernetes/original-index.gz
