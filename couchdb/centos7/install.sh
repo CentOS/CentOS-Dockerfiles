db0b5fefc4e143f11cbb5ba3f9ff6d378e6ed55a
mohammedzee1000 <mohammed.zee1000@gmail.com>
2016-10-19 13:36:34 +0530

    Added gpg key check for the couchdb tarball.

--- couchdb/centos7/install.sh
+++ couchdb/centos7/install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-COUCH_URL="https://www.apache.org/dist/couchdb/source/1.6.1/apache-couchdb-1.6.1.tar.gz"
-COUCH_CONFIG_DIR="/usr/local/etc/couchdb"
-COUCH_TAR="apache-couchdb-1.6.1.tar.gz"
-COUCH_DIR="apache-couchdb-1.6.1"
+COUCH_URL="https://www.apache.org/dist/couchdb/source/1.6.1/"
+COUCH_CONFIG_DIR="/usr/local/etc/couchdb/"
+COUCH_NAME="apache-couchdb-1.6.1"
+COUCH_TAR="${COUCH_NAME}.tar.gz"
+COUCH_DIR="${COUCH_NAME}"
+COUCH_ASC="${COUCH_TAR}.asc"
 COUCH_DEP="wget make autoconf autoconf-archive automake libtool perl-Test-Harness erlang libicu-devel js-devel curl-devel gcc-c++"
 CLEANUP_PKGS="wget make"
 
@@ -14,7 +16,17 @@ yum -y install epel-release;
 yum -y install ${COUCH_DEP} && yum clean all
 
 # Download and install couchdb
-wget ${COUCH_URL};
+# * Get the package and its gpgkey
+wget "${COUCH_URL}${COUCH_TAR}";
+wget "${COUCH_URL}${COUCH_ASC}"
+# * Verify the key
+echo "*******Verifying the gpgkey*******"
+gpg --keyserver pgpkeys.mit.edu --recv-key 04F4EE9B
+gpg --verify ${COUCH_ASC} ${COUCH_TAR}
+if [ $? -ne 0 ]; then
+	exit $?;
+fi
+# * Extract and install
 tar -xzf ${COUCH_TAR};
 cd ${COUCH_DIR};
 /bin/sh ./configure --with-erlang=/usr/lib64/erlang/usr/include;
@@ -32,9 +44,9 @@ chmod 0770 /usr/local/var/log/couchdb;
 chmod 0770 /usr/local/var/run/couchdb;
 
 # Configure couchdb to listen at 0.0.0.0
-sed -e 's/^bind_address = .*$/bind_address = 0.0.0.0/' -i "${COUCH_CONFIG_DIR}/default.ini"
+sed -e 's/^bind_address = .*$/bind_address = 0.0.0.0/' -i "${COUCH_CONFIG_DIR}default.ini"
 
 # Cleanup unnessasary stuff
 yum -y remove ${CLEANUP_PKGS} && yum clean all;
 cd ..;
-rm -rf ${COUCH_DIR}*
+rm -rf ${COUCH_NAME}*
