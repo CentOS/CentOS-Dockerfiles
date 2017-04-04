217deb826fee0f1da17ce0adb4b3b4aa256b7a46
mohammedzee1000 <mohammed.zee1000@gmail.com>
2017-04-04 17:58:39 +0530

    Added dockerfiles to create container for moodle.

---
+++ moodle/centos7/install.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+set -eux;
+
+# Initialize variables
+HTTPD_CONF="/etc/httpd/conf/httpd.conf"
+HTTPD_WELCOME="/etc/httpd/conf.d/welcome.conf"
+INSTALL_PKGS2="httpd nss_wrapper gettext";
+PHP_REQUIRED="php php-mysql php-pgsql php-xml php-xmlrpc php-gd"
+PHP_GOOD_TO_HAVE="php-xcache php-intl php-soap php-xmlrpc php-mbstring"
+PHP_PACKAGES="${PHP_REQUIRED} ${PHP_GOOD_TO_HAVE}"
+INSTALL_PKGS1="wget";
+INSTALL_PKGS="${INSTALL_PKGS1} ${INSTALL_PKGS2} ${PHP_PACKAGES}"
+MOODLE="moodle"
+MOODLE_DOWNLOAD_BASE="https://download.moodle.org"
+MOODLE_VERSION=${MOODLE_VERSION-"latest"}
+MOODLE_NODOT_VERSION=${MOODLE_NODOT_VERSION:-"moodle"}
+MOODLE_TAR="${MOODLE}-${MOODLE_VERSION}.tgz"
+MOODLE_DOWNLOAD_URL="${MOODLE_DOWNLOAD_BASE}/${MOODLE_NODOT_VERSION}/${MOODLE_TAR}"
+PHP_INI_LOC="/etc/php.ini"
+
+export MOODLE_DATA="/var/moodledata"
+
+# Setup necessary packages
+yum -y install epel-release && yum -y install --skip-broken ${INSTALL_PKGS};
+
+# Install moodle
+pushd /var/www;
+wget ${MOODLE_DOWNLOAD_URL} && tar zxvf ${MOODLE_TAR} && mv /var/www/${MOODLE}/* /var/www/html;
+mkdir -p /var/moodledata
+popd;
+
+# Fixup Configurations
+rm -rf ${HTTPD_WELCOME};
+sed -i 's/^Listen 80/Listen 8080\\\nListen 8443/g' ${HTTPD_CONF};
+sed -i 's/^Listen 8080\\/Listen 8080/g' ${HTTPD_CONF};
+sed -i 's/^Group apache/Group root/g' ${HTTPD_CONF};
+mkdir -p /etc/httpd/logs && touch /etc/httpd/logs/error_log && touch /etc/httpd/logs/access_log;
+
+# Fix the permissions
+for item in "/etc/httpd" "/var/www" "/var/moodledata"; do
+    . /opt/scripts/fix-permissions.sh ${item} apache;
+done
+
+chmod -R 777 /etc/httpd/logs;
+
+# Enable opcache
+cat <<EOT >> ${PHP_INI_LOC}
+[opcache]
+opcache.enable=1
+opcache.enable_cli=1
+opcache.memory_consumption=128
+opcache.interned_strings_buffer=8
+opcache.max_accelerated_files=4000
+opcache.max_wasted_percentage=5
+opcache.use_cwd=1
+opcache.validate_timestamps=1
+opcache.fast_shutdown=1
+EOT
+
+# Cleanup
+yum -y remove ${INSTALL_PKGS1} && yum clean all
---
+++ moodle/centos7/run.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+export USER_ID=$(id -u);
+export GROUP_ID=$(id -g);
+envsubst < /opt/scripts/passwd.template > /tmp/passwd;
+export LD_PRELOAD=libnss_wrapper.so;
+export NSS_WRAPPER_PASSWD=/tmp/passwd;
+export NSS_WRAPPER_GROUP=/etc/group;
+
+DB_TYPE=${DB_TYPE:-"mysqli"};
+DB_LIBRARY=${DB_LIBRARY:-"native"};
+DB_HOST=${DB_HOST:-"localhost"};
+DB_NAME=${DB_NAME:-"moodle"};
+DB_USER=${DB_USER:-"moodle"};
+DB_PASSWD=${DB_PASSWD:-"moodle"};
+if [ -z ${MOODLE_HOST} ]; then
+    MOODLE_HOST="`hostname -i`";
+fi
+
+if [ $1 == "moodle" ]; then
+    sed -e "s/pgsql/${DB_TYPE}/
+    s/username/${DB_USER}/
+    s/password/${DB_PASSWD}/
+    s/http:\/\/example.com\/moodle/http:\/\/${MOODLE_HOST}:8080/
+    s/localhost/${DB_HOST}/
+    s/\/home\/example\/moodledata/\/var\/moodledata/" /var/www/html/config-dist.php > /var/www/html/config.php;
+
+    exec /usr/sbin/httpd -DFOREGROUND;
+else
+    exec $@
+fi
