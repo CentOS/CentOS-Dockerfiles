1e3848cbb26ad4f1cb371bc0a3570af131067c2e
mohammedzee1000 <mohammed.zee1000@gmail.com>
2017-04-05 17:13:33 +0530

    Added redis, solr apcu packages.

--- moodle/centos7/install.sh
+++ moodle/centos7/install.sh
@@ -9,30 +9,33 @@ HTTPD_WELCOME="/etc/httpd/conf.d/welcome.conf"
 INSTALL_PKGS1="wget";
 INSTALL_PKGS2="httpd nss_wrapper gettext";
 
-PHP_PACKAGES1="php php-mysql php-pgsql php-xml php-xmlrpc php-gd php-mongodb php-pecl-apcu mssql-tools redis freetds";
+PHP_PACKAGES1="php php-mysql php-pgsql php-xml php-xmlrpc php-gd php-mongodb php-pecl-apcu redis freetds";
 PHP_PACKAGES2="php-pecl-zendopcache php-pecl-memcache php-pecl-memcached php-intl php-soap php-xmlrpc php-mbstring";
-PHP_PACKAGES3="php-pecl-solr php-pecl-solr2 "
+PHP_PACKAGES3="php-pecl-solr php-pecl-solr2 php-pecl-redis"
+PHP_MS_ORACLE_PACKAGES="mssql-tools"
 
-PHP_PACKAGES="${PHP_PACKAGES1} ${PHP_PACKAGES2} ${PHP_PACKAGES3}"
-INSTALL_PKGS="${INSTALL_PKGS1} ${INSTALL_PKGS2} ${PHP_PACKAGES}"
+PHP_PACKAGES="${PHP_PACKAGES1} ${PHP_PACKAGES2} ${PHP_PACKAGES3}";
+INSTALL_PKGS="${INSTALL_PKGS1} ${INSTALL_PKGS2} ${PHP_PACKAGES}";
 
-MOODLE="moodle"
-MOODLE_DOWNLOAD_BASE="https://download.moodle.org"
-MOODLE_VERSION=${MOODLE_VERSION-"latest"}
-MOODLE_NODOT_VERSION=${MOODLE_NODOT_VERSION:-"moodle"}
-MOODLE_TAR="${MOODLE}-${MOODLE_VERSION}.tgz"
-MOODLE_DOWNLOAD_URL="${MOODLE_DOWNLOAD_BASE}/${MOODLE_NODOT_VERSION}/${MOODLE_TAR}"
-OPCACHE_INI_LOC="/etc/php.d/opcache.ini"
-MSSQL_REPODATA="https://packages.microsoft.com/config/rhel/7/prod.repo"
+MOODLE="moodle";
+MOODLE_DOWNLOAD_BASE="https://download.moodle.org";
+MOODLE_VERSION=${MOODLE_VERSION-"latest"};
+MOODLE_NODOT_VERSION=${MOODLE_NODOT_VERSION:-"moodle"};
+MOODLE_TAR="${MOODLE}-${MOODLE_VERSION}.tgz";
+MOODLE_DOWNLOAD_URL="${MOODLE_DOWNLOAD_BASE}/${MOODLE_NODOT_VERSION}/${MOODLE_TAR}";
+OPCACHE_INI_LOC="/etc/php.d/opcache.ini";
+MSSQL_REPODATA="https://packages.microsoft.com/config/rhel/7/prod.repo";
 
-export ACCEPT_EULA="Y"
-export MOODLE_DATA="/var/moodledata"
+export ACCEPT_EULA="Y";
+export MOODLE_DATA="/var/moodledata";
+
+# INSTALL BEGINS
 
 # Setup repositories
 pushd /etc/yum.repos.d && wget ${MSSQL_REPODATA} && popd
 
 # Setup necessary packages
-yum -y install epel-release && yum -y install --skip-broken ${INSTALL_PKGS};
+yum -y install epel-release && yum -y install --skip-broken ${INSTALL_PKGS}
 
 # Install moodle
 pushd /var/www;
@@ -48,7 +51,7 @@ sed -i 's/^Group apache/Group root/g' ${HTTPD_CONF};
 mkdir -p /etc/httpd/logs && touch /etc/httpd/logs/error_log && touch /etc/httpd/logs/access_log;
 
 # Fix the permissions
-for item in "/etc/httpd" "/var/www" "/var/moodledata"; do
+for item in "/etc/httpd" "/var/www" "/var/moodledata" "/etc/freetds.conf"; do
     . /opt/scripts/fix-permissions.sh ${item} apache;
 done
 
--- moodle/centos7/run.sh
+++ moodle/centos7/run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# Permissions
 export USER_ID=$(id -u);
 export GROUP_ID=$(id -g);
 envsubst < /opt/scripts/passwd.template > /tmp/passwd;
@@ -7,6 +8,7 @@ export LD_PRELOAD=libnss_wrapper.so;
 export NSS_WRAPPER_PASSWD=/tmp/passwd;
 export NSS_WRAPPER_GROUP=/etc/group;
 
+# Initialize and validate variables
 DB_TYPE=${DB_TYPE:-"pgsql"};
 DB_LIBRARY=${DB_LIBRARY:-"native"};
 DB_HOST=${DB_HOST:-"localhost"};
@@ -23,13 +25,40 @@ if [ -z ${MOODLE_HOST_NOPORT} ]; then
     fi
 fi
 
+FREETDS_FILE="/etc/freetds.conf";
+FREETDS_VERSION="4.2";
+
+# Helpers
+
+mssql_freetds() {
+
+cat <<EOT > ${FREETDS_FILE}
+[global]
+    tds version = ${FREETDS_VERSION}
+    text size = 20971520
+    host = ${DB_HOST}
+    port = 1433
+    client charset = UTF-8
+EOT
+
+}
+
+# Main Begins
+
 if [ $1 == "moodle" ]; then
+
+    if [ ${DB_TYPE} == "mssql" ]; then
+        echo "The packages required for this require PHP 7, hence MSSQL is not currently supported";
+        exit 1
+        # mssql_freetds;
+    fi
     sed -e "s/pgsql/${DB_TYPE}/
     s/username/${DB_USER}/
     s/password/${DB_PASSWD}/
     s/http:\/\/example.com\/moodle/http:\/\/${MOODLE_HOST}/
     s/localhost/${DB_HOST}/
     s/\/home\/example\/moodledata/\/var\/moodledata/" /var/www/html/config-dist.php > /var/www/html/config.php;
+
 
     exec /usr/sbin/httpd -DFOREGROUND;
 else
