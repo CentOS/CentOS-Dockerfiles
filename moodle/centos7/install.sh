19b39cb78e01dd49cfa2a6dd0b8cad1d429bbc42
mohammedzee1000 <mohammed.zee1000@gmail.com>
2017-04-06 14:51:03 +0530

    Updated to use remicolt repository for php 71.

--- moodle/centos7/install.sh
+++ moodle/centos7/install.sh
@@ -8,14 +8,21 @@ HTTPD_WELCOME="/etc/httpd/conf.d/welcome.conf"
 
 INSTALL_PKGS1="wget";
 INSTALL_PKGS2="httpd nss_wrapper gettext";
+REMI_REPO="http://rpms.famillecollet.com/enterprise/remi-release-7.rpm"
+PHP_REMI_VERSION="${PHP_REMI_VERSION:-"php71"}"
 
-PHP_PACKAGES1="php php-mysql php-pgsql php-xml php-xmlrpc php-gd php-mongodb php-pecl-apcu redis freetds";
-PHP_PACKAGES2="php-pecl-zendopcache php-pecl-memcache php-pecl-memcached php-intl php-soap php-xmlrpc php-mbstring";
-PHP_PACKAGES3="php-pecl-solr php-pecl-solr2 php-pecl-redis"
-PHP_MS_ORACLE_PACKAGES="mssql-tools"
+PHP1="${PHP_REMI_VERSION} ${PHP_REMI_VERSION}-php ${PHP_REMI_VERSION}-php-pgsql ${PHP_REMI_VERSION}-php-mysqlnd"
+PHP2="${PHP_REMI_VERSION}-php-pecl-mysql ${PHP_REMI_VERSION}-php-xml ${PHP_REMI_VERSION}-php-xmlrpc "
+PHP3="${PHP_REMI_VERSION}-php-gd ${PHP_REMI_VERSION}-php-pecl-mongodb ${PHP_REMI_VERSION}php-pecl-apcu"
+PHP4="${PHP_REMI_VERSION}-php-pecl-apcu-bc ${PHP_REMI_VERSION}-php-pecl-redis ${PHP_REMI_VERSION}-php-phpiredis"
+PHP5="${PHP_REMI_VERSION}-php-opcache ${PHP_REMI_VERSION}-php-pecl-memcache ${PHP_REMI_VERSION}-php-pecl-memcached"
+PHP6="${PHP_REMI_VERSION}-php-intl ${PHP_REMI_VERSION}-php-mbstring ${PHP_REMI_VERSION}-php-pecl-solr2"
+PHP7="${PHP_REMI_VERSION}-php-pecl-zip ${PHP_REMI_VERSION}-php-soap"
 
-PHP_PACKAGES="${PHP_PACKAGES1} ${PHP_PACKAGES2} ${PHP_PACKAGES3}";
-INSTALL_PKGS="${INSTALL_PKGS1} ${INSTALL_PKGS2} ${PHP_PACKAGES}";
+#PHP_MS_ORACLE_PACKAGES="freetds mssql-tools"
+
+PHP_PACKAGES="${PHP1} ${PHP2} ${PHP3} ${PHP4} ${PHP5} ${PHP6} ${PHP7}";
+INSTALL_PKGS="${INSTALL_PKGS2} ${PHP_PACKAGES}";
 
 MOODLE="moodle";
 MOODLE_DOWNLOAD_BASE="https://download.moodle.org";
@@ -23,7 +30,6 @@ MOODLE_VERSION=${MOODLE_VERSION-"latest"};
 MOODLE_NODOT_VERSION=${MOODLE_NODOT_VERSION:-"moodle"};
 MOODLE_TAR="${MOODLE}-${MOODLE_VERSION}.tgz";
 MOODLE_DOWNLOAD_URL="${MOODLE_DOWNLOAD_BASE}/${MOODLE_NODOT_VERSION}/${MOODLE_TAR}";
-OPCACHE_INI_LOC="/etc/php.d/opcache.ini";
 MSSQL_REPODATA="https://packages.microsoft.com/config/rhel/7/prod.repo";
 
 export ACCEPT_EULA="Y";
@@ -32,15 +38,16 @@ export MOODLE_DATA="/var/moodledata";
 # INSTALL BEGINS
 
 # Setup repositories
+yum -y install ${INSTALL_PKGS1}
 pushd /etc/yum.repos.d && wget ${MSSQL_REPODATA} && popd
 
 # Setup necessary packages
-yum -y install epel-release && yum -y install --skip-broken ${INSTALL_PKGS}
+yum -y install epel-release && yum -y install ${REMI_REPO} && yum -y install --skip-broken ${INSTALL_PKGS}
 
 # Install moodle
 pushd /var/www;
 wget ${MOODLE_DOWNLOAD_URL} && tar zxvf ${MOODLE_TAR} && mv /var/www/${MOODLE}/* /var/www/html;
-mkdir -p /var/moodledata
+mkdir -p /var/moodledata && rm -rf ${MOODLE_TAR}
 popd;
 
 # Fixup Configurations
@@ -51,26 +58,11 @@ sed -i 's/^Group apache/Group root/g' ${HTTPD_CONF};
 mkdir -p /etc/httpd/logs && touch /etc/httpd/logs/error_log && touch /etc/httpd/logs/access_log;
 
 # Fix the permissions
-for item in "/etc/httpd" "/var/www" "/var/moodledata" "/etc/freetds.conf"; do
+for item in "/etc/httpd" "/var/www" "/var/moodledata"; do
     . /opt/scripts/fix-permissions.sh ${item} apache;
 done
 
 chmod -R 777 /etc/httpd/logs;
-
-# Enable opcache
-cat <<EOT > ${OPCACHE_INI_LOC}
-[opcache]
-opcache.enable = 1
-opcache.memory_consumption = 64
-opcache.max_accelerated_files = 8000
-opcache.revalidate_freq = 60
-
-; Required for Moodle
-opcache.use_cwd = 1
-opcache.validate_timestamps = 1
-opcache.save_comments = 1
-opcache.enable_file_override = 0
-EOT
 
 # Cleanup
 yum -y remove ${INSTALL_PKGS1} && yum clean all
