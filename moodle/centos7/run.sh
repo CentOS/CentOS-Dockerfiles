0f4df784f3ade10b3e3cfde648395ee1a75aac74
mohammedzee1000 <mohammed.zee1000@gmail.com>
2017-04-06 19:21:25 +0530

    Updated to single env for hostand fixed name.
    Fixed the spacing.

--- moodle/centos7/run.sh
+++ moodle/centos7/run.sh
@@ -15,15 +15,7 @@ DB_HOST=${DB_HOST:-"localhost"};
 DB_NAME=${DB_NAME:-"moodle"};
 DB_USER=${DB_USER:-"moodle"};
 DB_PASSWD=${DB_PASSWD:-"moodle"};
-if [ -z ${MOODLE_HOST} ]; then
-    MOODLE_HOST="`hostname -i`";
-fi
-
-if [ -z ${MOODLE_HOST_NOPORT} ]; then
-    if [[ ${MOODLE_HOST} != *":"* ]]; then
-        MOODLE_HOST="${MOODLE_HOST}:8080"
-    fi
-fi
+MOODLE_URL=${MOODLE_URL:-"http://`hostname -i`:8080"}
 
 # Main Begins
 
@@ -33,13 +25,13 @@ if [ $1 == "moodle" ]; then
         echo "Oracle database is not currently supported due to licensing issues with their client";
         exit 1
     fi
+
     sed -e "s/pgsql/${DB_TYPE}/
     s/username/${DB_USER}/
     s/password/${DB_PASSWD}/
-    s/http:\/\/example.com\/moodle/http:\/\/${MOODLE_HOST}/
+    s/http:\/\/example.com\/moodle/${MOODLE_URL}/
     s/localhost/${DB_HOST}/
     s/\/home\/example\/moodledata/\/var\/moodledata/" /var/www/html/config-dist.php > /var/www/html/config.php;
-
 
     exec /usr/sbin/httpd -DFOREGROUND;
 else
