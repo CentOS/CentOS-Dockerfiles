54944d7be022aa06eaedbf4cc18cdeed4305f049
Jason Brooks <jbrooks@redhat.com>
2017-09-06 08:26:19 -0700

    remove offending kubelet args

--- kubernetes/kubelet/launch.sh
+++ kubernetes/kubelet/launch.sh
@@ -3,7 +3,7 @@
 [ -f /etc/kubernetes/kubelet ] && source /etc/kubernetes/kubelet
 [ -f /etc/kubernetes/config ] && source /etc/kubernetes/config
 
-TEMP_KUBELET_ARGS='--cgroup-driver=systemd --cgroups-per-qos=false --enforce-node-allocatable='
+TEMP_KUBELET_ARGS='--cgroup-driver=systemd'
 
 ARGS="$@ $TEMP_KUBELET_ARGS $KUBE_LOGTOSTDERR $KUBE_LOG_LEVEL $KUBELET_API_SERVER $KUBELET_ADDRESS $KUBELET_PORT $KUBELET_HOSTNAME $KUBE_ALLOW_PRIV $KUBELET_ARGS"
 
